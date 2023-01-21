17e3d3d4db6860ae0e500c54ccb4bcf0e23f6252
arlomd8 <arlomariod@gmail.com>
2023-01-21 14:59:32 +0700

    Update Material, UI LookAtCamera, Animation Network, UI Dynamic
    Next
    5. Player Room Tiap Content
    6. Voice Photon
    7. Networking Value RandomAvatar
    8. Networking Value Player Name Counter

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Unity.XR.CoreUtils;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    private PhotonView photonView;
+
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    public Animator rightHandAnimator;
+    public Animator leftHandAnimator;
+
+    public List<GameObject> ava;
+    int randomAva;
+
+    public Transform headRig;
+    public Transform leftHandRig;
+    public Transform rightHandRig;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        RandomAvatar();
+        SetupRig();
+
+
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            head.gameObject.SetActive(false);
+            ava[randomAva].SetActive(false);
+
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+            AnimateHand(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+            AnimateHand(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+        }
+    }
+
+    void AnimateHand(InputDevice inputDevice, Animator animator)
+    {
+        inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float pointValue);
+        inputDevice.TryGetFeatureValue(CommonUsages.grip, out float grabValue);
+
+        animator.SetFloat("Point", pointValue);
+        animator.SetFloat("Grab", grabValue);
+
+        //if (inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float pointValue))
+        //{
+        //    animator.SetFloat("Point", pointValue);
+        //}
+        //else
+        //{
+        //    animator.SetFloat("Point", 0);
+        //}
+
+        //if (inputDevice.TryGetFeatureValue(CommonUsages.grip, out float grabValue))
+        //{
+        //    animator.SetFloat("Grab", grabValue);
+        //}
+        //else
+        //{
+        //    animator.SetFloat("Grab", 0);
+        //}
+    }
+
+    private void MapPosition(Transform target, Transform rigTransform)
+    {
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
+    }
+
+    void RandomAvatar()
+    {
+        randomAva = Random.Range(0, ava.Count);
+        ava[randomAva].SetActive(true);
+        print(randomAva);
+    }
+
+    void SetupRig()
+    {
+        XROrigin rig = FindObjectOfType<XROrigin>();
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+    }
+}
--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -5,15 +5,11 @@ using Photon.Pun;
 
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
-    public List<GameObject> Spawns = new List<GameObject>();
     public GameObject spawnedPlayer;
-    int randomAvatar;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        randomAvatar = Random.Range(0, Spawns.Count);
-        //spawnedPlayer = PhotonNetwork.Instantiate(Spawns[randomAvatar].name, transform.position, transform.rotation);
         spawnedPlayer = PhotonNetwork.Instantiate("Avatar", transform.position, transform.rotation);
     }
 
