0ee9341d4500440a1b2d47ff2f59f9f6ed64e4db
arlomd8 <arlomariod@gmail.com>
2023-01-22 18:29:20 +0700

    Need Fixing for Get Nickname (?)
    Bisa Pakai GetPlayerNumber? or CurrentRoom Players

--- Assets/Scripts/GetNickname.cs
+++ Assets/Scripts/GetNickname.cs
@@ -8,7 +8,7 @@ public class GetNickname : MonoBehaviourPunCallbacks
 {
     private void Start()
     {
-        photonView.Owner.NickName = "PLAYER " + photonView.Owner.ActorNumber;
+        photonView.Owner.NickName = "PLAYER " + photonView.Owner.ActorNumber; //Bisa Pakai GetPlayerNumber? or CurrentRoom Players
         GetComponent<TextMeshProUGUI>().text = photonView.Owner.NickName;
     }
 }
